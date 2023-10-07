d0a79ebe45e15347f4895413ecd825a27f43acc3
hyunjeong.kim <hyunjeong.kim@phloxcorp.io>
2023-10-08 00:00:13 +0900

    handler 수정

--- handler.ts
+++ handler.ts
@@ -1,9 +1,22 @@
+import mainTwit from './util/mainTwit';
+import { TICKETING_SITE } from './type/types';
+import alert from './util/alert'
 
 export async function xia() {
+
+  const scheduleList = [
+    { time: new Date(2023, 9, 7, 14), sites: [TICKETING_SITE.LOTTE] },
+    { time: new Date(2023, 9, 8, 13), sites: [TICKETING_SITE.TOPING_FIRST] },
+    { time: new Date(2023, 9, 8, 14), sites: [TICKETING_SITE.INTERPARK, TICKETING_SITE.TICKETLINK] },
+  ]
+
+  console.log(mainTwit('드라큘라', '1st', scheduleList))
+  await alert('드라큘라', scheduleList)
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'success',
     }),
   };
   return response
