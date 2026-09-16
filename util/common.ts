28734bac22a2cd562a6d38a16c58d4860cf6950c
hyunjeong.kim <hyunjeong.kim@phloxcorp.io>
2026-09-16 18:00:09 +0900

    알림에서 UTC 시간으로 표시되는 버그 수정

--- util/common.ts
+++ util/common.ts
@@ -1,10 +1,11 @@
 function format(time: Date) {
-  const month = (time.getMonth() + 1).toString()
-  const date = time.getDate().toString()
+  const kst = new Date(time.getTime() + 9 * 60 * 60 * 1000)
+  const month = (kst.getUTCMonth() + 1).toString()
+  const date = kst.getUTCDate().toString()
   const WEEKDAY = ['일', '월', '화', '수', '목', '금', '토']
-  const day = WEEKDAY[time.getDay()]
-  const hour = time.getHours().toString().padStart(2, '0')
-  const minute = time.getMinutes().toString().padStart(2, '0')
+  const day = WEEKDAY[kst.getUTCDay()]
+  const hour = kst.getUTCHours().toString().padStart(2, '0')
+  const minute = kst.getUTCMinutes().toString().padStart(2, '0')
 
   return `${month}/${date}(${day}) ${hour}:${minute}`
 }
